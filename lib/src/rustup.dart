dc9c19f045675e16a44ef5181e7475a732caed65
Matej Knopp <matej.knopp@gmail.com>
2024-03-03 19:32:26 +0100

    feat: find rustup executable

---
+++ lib/src/rustup.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+import 'package:path/path.dart' as path;
+
+class Rustup {
+  /// Returns the path to the `rustup` executable, or `null` if it is not found.
+  static String? executablePath() {
+    final envPath = Platform.environment['PATH'];
+    final envPathSeparator = Platform.isWindows ? ';' : ':';
+    final home = Platform.isWindows
+        ? Platform.environment['USERPROFILE']
+        : Platform.environment['HOME'];
+    final paths = [
+      if (home != null) path.join(home, '.cargo', 'bin'),
+      if (envPath != null) ...envPath.split(envPathSeparator),
+    ];
+    for (final p in paths) {
+      final rustup = Platform.isWindows ? 'rustup.exe' : 'rustup';
+      final rustupPath = path.join(p, rustup);
+      if (File(rustupPath).existsSync()) {
+        return rustupPath;
+      }
+    }
+    return null;
+  }
+}
